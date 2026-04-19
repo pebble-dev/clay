8fc3c3c276a2708e7470203414bbb3f0f4553b72
Joseph Mearman <joseph@mearman.co.uk>
2026-04-19 10:39:08 +0100

    refactor: convert manipulators to TypeScript
    Define ManipulatorContext and Manipulator interfaces. Type all get/set
    and shared disable/enable/hide/show methods with explicit this parameter.

--- src/scripts/lib/manipulators.ts
+++ src/scripts/lib/manipulators.ts
@@ -1,180 +1,197 @@
 'use strict';
 
-var _ = require('../vendor/minified')._;
+import minified = require('../vendor/minified');
+const _ = minified._;
 
-/**
- * @returns {ClayItem|ClayEvents}
- * @extends {ClayItem}
- */
-function disable() {
+interface M {
+  [index: number]: HTMLElement;
+  length: number;
+  add(child: M | string): M;
+  set(property: string, value?: unknown): M;
+  set(cssObj: Record<string, unknown>): M;
+  get(property: string): unknown;
+  select(selector: string): M;
+  on(events: string, handler: (...args: unknown[]) => void): M;
+  off(handler: (...args: unknown[]) => void): M;
+  trigger(name: string, eventObj?: unknown): M;
+  each(callback: (element: HTMLElement, index: number) => void): M;
+}
+
+interface ManipulatorContext {
+  $element: M;
+  $manipulatorTarget: M;
+  trigger(name: string, eventObj?: unknown): ManipulatorContext;
+  get(): unknown;
+  roundColorToLayout?(value: number): number;
+}
+
+// Shared manipulator methods
+function disable(this: ManipulatorContext): ManipulatorContext {
   if (this.$manipulatorTarget.get('disabled')) { return this; }
   this.$element.set('+disabled');
   this.$manipulatorTarget.set('disabled', true);
   return this.trigger('disabled');
 }
 
-/**
- * @returns {ClayItem|ClayEvents}
- * @extends {ClayItem}
- */
-function enable() {
+function enable(this: ManipulatorContext): ManipulatorContext {
   if (!this.$manipulatorTarget.get('disabled')) { return this; }
   this.$element.set('-disabled');
   this.$manipulatorTarget.set('disabled', false);
   return this.trigger('enabled');
 }
 
-/**
- * @returns {ClayItem|ClayEvents}
- * @extends {ClayItem}
- */
-function hide() {
+function hide(this: ManipulatorContext): ManipulatorContext {
   if (this.$element[0].classList.contains('hide')) { return this; }
   this.$element.set('+hide');
   return this.trigger('hide');
 }
 
-/**
- * @returns {ClayItem|ClayEvents}
- * @extends {ClayItem}
- */
-function show() {
+function show(this: ManipulatorContext): ManipulatorContext {
   if (!this.$element[0].classList.contains('hide')) { return this; }
   this.$element.set('-hide');
   return this.trigger('show');
 }
 
-module.exports = {
+interface Manipulator {
+  get(this: ManipulatorContext): unknown;
+  set(this: ManipulatorContext, value: unknown): ManipulatorContext;
+  disable?: (this: ManipulatorContext) => ManipulatorContext;
+  enable?: (this: ManipulatorContext) => ManipulatorContext;
+  hide?: (this: ManipulatorContext) => ManipulatorContext;
+  show?: (this: ManipulatorContext) => ManipulatorContext;
+}
+
+export = {
   html: {
-    get: function() {
+    get(this: ManipulatorContext) {
       return this.$manipulatorTarget.get('innerHTML');
     },
-    set: function(value) {
-      if (this.get() === value.toString(10)) { return this; }
+    set(this: ManipulatorContext, value: unknown) {
+      if (this.get() === String(value)) { return this; }
       this.$manipulatorTarget.set('innerHTML', value);
       return this.trigger('change');
     },
-    hide: hide,
-    show: show
+    hide,
+    show
   },
   button: {
-    get: function() {
+    get(this: ManipulatorContext) {
       return this.$manipulatorTarget.get('innerHTML');
     },
-    set: function(value) {
-      if (this.get() === value.toString(10)) { return this; }
+    set(this: ManipulatorContext, value: unknown) {
+      if (this.get() === String(value)) { return this; }
       this.$manipulatorTarget.set('innerHTML', value);
       return this.trigger('change');
     },
-    disable: disable,
-    enable: enable,
-    hide: hide,
-    show: show
+    disable,
+    enable,
+    hide,
+    show
   },
   val: {
-    get: function() {
+    get(this: ManipulatorContext) {
       return this.$manipulatorTarget.get('value');
     },
-    set: function(value) {
-      if (this.get() === value.toString(10)) { return this; }
+    set(this: ManipulatorContext, value: unknown) {
+      if (this.get() === String(value)) { return this; }
       this.$manipulatorTarget.set('value', value);
       return this.trigger('change');
     },
-    disable: disable,
-    enable: enable,
-    hide: hide,
-    show: show
+    disable,
+    enable,
+    hide,
+    show
   },
   slider: {
-    get: function() {
-      return parseFloat(this.$manipulatorTarget.get('value'));
+    get(this: ManipulatorContext) {
+      return parseFloat(this.$manipulatorTarget.get('value') as string);
     },
-    set: function(value) {
-      var initVal = this.get();
+    set(this: ManipulatorContext, value: unknown) {
+      const initVal = this.get();
       this.$manipulatorTarget.set('value', value);
       if (this.get() === initVal) { return this; }
       return this.trigger('change');
     },
-    disable: disable,
-    enable: enable,
-    hide: hide,
-    show: show
+    disable,
+    enable,
+    hide,
+    show
   },
   checked: {
-    get: function() {
+    get(this: ManipulatorContext) {
       return this.$manipulatorTarget.get('checked');
     },
-    set: function(value) {
+    set(this: ManipulatorContext, value: unknown) {
       if (!this.get() === !value) { return this; }
       this.$manipulatorTarget.set('checked', !!value);
       return this.trigger('change');
     },
-    disable: disable,
-    enable: enable,
-    hide: hide,
-    show: show
+    disable,
+    enable,
+    hide,
+    show
   },
   radiogroup: {
-    get: function() {
+    get(this: ManipulatorContext) {
       return this.$element.select('input:checked').get('value');
     },
-    set: function(value) {
-      if (this.get() === value.toString(10)) { return this; }
+    set(this: ManipulatorContext, value: unknown) {
+      if (this.get() === String(value)) { return this; }
       this.$element
-        .select('input[value="' + value.replace('"', '\\"') + '"]')
+        .select('input[value="' + (value as string).replace('"', '\\"') + '"]')
         .set('checked', true);
       return this.trigger('change');
     },
-    disable: disable,
-    enable: enable,
-    hide: hide,
-    show: show
+    disable,
+    enable,
+    hide,
+    show
   },
   checkboxgroup: {
-    get: function() {
-      var result = [];
-      this.$element.select('input').each(function(item) {
-        result.push(!!item.checked);
+    get(this: ManipulatorContext) {
+      const result: boolean[] = [];
+      this.$element.select('input').each(function(item: HTMLElement) {
+        result.push(!!(item as HTMLInputElement).checked);
       });
       return result;
     },
-    set: function(values) {
-      var self = this;
-      values = Array.isArray(values) ? values : [];
+    set(this: ManipulatorContext, values: unknown) {
+      const self = this;
+      let valuesArray = Array.isArray(values) ? values : [];
 
-      while (values.length < this.get().length) {
-        values.push(false);
+      while (valuesArray.length < (this.get() as boolean[]).length) {
+        valuesArray.push(false);
       }
 
-      if (_.equals(this.get(), values)) { return this; }
+      if (_.equals(this.get(), valuesArray)) { return this; }
 
       self.$element.select('input')
         .set('checked', false)
-        .each(function(item, index) {
-          item.checked = !!values[index];
+        .each(function(item: HTMLElement, index: number) {
+          (item as HTMLInputElement).checked = !!valuesArray[index];
         });
 
       return self.trigger('change');
     },
-    disable: disable,
-    enable: enable,
-    hide: hide,
-    show: show
+    disable,
+    enable,
+    hide,
+    show
   },
   color: {
-    get: function() {
-      return parseInt(this.$manipulatorTarget.get('value'), 10) || 0;
+    get(this: ManipulatorContext) {
+      return parseInt(this.$manipulatorTarget.get('value') as string, 10) || 0;
     },
-    set: function(value) {
-      value = this.roundColorToLayout(value || 0);
+    set(this: ManipulatorContext, value: unknown) {
+      const roundedValue = this.roundColorToLayout ? this.roundColorToLayout(value as number || 0) : (value as number || 0);
 
-      if (this.get() === value) { return this; }
-      this.$manipulatorTarget.set('value', value);
+      if (this.get() === roundedValue) { return this; }
+      this.$manipulatorTarget.set('value', roundedValue);
       return this.trigger('change');
     },
-    disable: disable,
-    enable: enable,
-    hide: hide,
-    show: show
+    disable,
+    enable,
+    hide,
+    show
   }
-};
+} as Record<string, Manipulator>;
